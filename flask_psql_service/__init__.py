a3765b015472adf206a6b01f5adffea54344d9b3
TravisAucoin <travis_aucoin@stuller.com>
2022-04-15 23:00:00 -0500

    Added new model and a super simple rest api

--- flask_psql_service/__init__.py
+++ flask_psql_service/__init__.py
@@ -2,14 +2,15 @@ __version__ = '0.1.0'
 
 # third-party imports
 import json
-from flask import Flask
+from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-
 # local imports
 from config import app_config
+
 # db variable initialization
 db = SQLAlchemy()
+
 
 def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
@@ -21,9 +22,26 @@ def create_app(config_name):
     @app.route('/')
     def hello_world():
         return 'Hello, World!'
-    
-    migrate = Migrate(app, db)
 
+    @app.route('/person/<id>', methods=['GET'])
+    def get_person(id):
+        # TODO: figure out a way around circular ref here
+        from .models import Person
+        person = Person.query.get(id)
+        del person.__dict__['_sa_instance_state']
+        return jsonify(person.__dict__)
+
+    @app.route('/person/create', methods=['POST'])
+    def create_person():
+        # TODO: figure out a way around circular ref here
+        from .models import Person
+        body = request.get_json()
+        person = Person(body['first_name'], body['last_name'], body['age'])
+        db.session.add(person)
+        db.session.commit()
+        return 'person created'
+
+    migrate = Migrate(app, db)
     from flask_psql_service import models
 
     return app
--- flask_psql_service/models.py
+++ flask_psql_service/models.py
@@ -17,3 +17,17 @@ class Result(db.Model):
 
     def __repr__(self):
         return '<id {}>'.format(self.id)
+
+
+class Person(db.Model):
+    __tablename__ = 'persons'
+
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(), nullable=False)
+    last_name = db.Column(db.String(), nullable=False)
+    age = db.Column(db.Integer(), nullable=False)
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
